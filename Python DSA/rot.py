47372228c55d3d5b0efdca014f303ed82cfd5718
Typher7 <111155443+Typher7@users.noreply.github.com>
2024-10-13 15:43:33 -0500

    fix

--- Python DSA/rot.py
+++ Python DSA/rot.py
@@ -1,14 +1,11 @@
 def encrypt_string(text):
     encrypted = ""
-    step = 1
-    for c in text:
-        if c.isalpha():
-            if c.isupper():
-                encrypted += (chr(64 + step + (ord(c) - 64) % 26))
-            else:
-                encrypted += (chr(96 + step + (ord(c) - 96) % 26)) 
+    shift = 1
+    for char in text:
+        if char.isalpha():
+            encrypted += chr(64 + (ord(char) - 64 + shift) % 26) if char.isupper() else chr(96 + (ord(char) - 96 + shift) % 26)
         else:
-            encrypted += c
+            encrypted += char
         # TODO: Check if `c` is a letter different from 'z' and 'Z'. If so, increment by 1.
         # If 'c' is 'z', change it to 'a'. If 'c' is 'Z', change it to 'A'.
         # Otherwise, keep 'c' unchanged and add it to the encrypted list.
@@ -17,3 +14,4 @@ def encrypt_string(text):
 # Encrypt the string "Hello, Python!" by shifting each letter in the alphabet one by one.
 encrypted_text = encrypt_string("Hello, Python!")
 print("The encrypted text is:", encrypted_text) # Should print out "Ifmmp, Qzuipo!"
+
