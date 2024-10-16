78a16c27e60e8e783b37175ad8a144ffc2a5c256
Typher7 <111155443+Typher7@users.noreply.github.com>
2024-10-16 14:39:09 -0500

    min cars required

---
+++ Python DSA/minCars.py
@@ -0,0 +1,12 @@
+#test case 1: Persons = [1,4,1], Seats = [1,5,1], returns 2
+
+def solution(persons, seats):
+    seats.sort(reverse=True)
+    total_persons = sum(persons)
+    
+    for i in range(len(seats)):
+        total_persons -= seats[i]
+        if total_persons <= 0:
+            return i + 1
+    return 0
+print("Min cars: ", solution([2,3,4,2], [2,5,7,2]))
